5b4d417846d6d34a4797c4b91ca9df57bbda833e
EGARRISXN <egarrisxn@gmail.com>
2024-01-25 04:35:18 -0500

    tweak.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import SocialIcons from "@/lib/socialIcons";
 export default function Footer() {
   return (
     <div className="w-full text-center p-4 flex items-center justify-between mx-auto border-t">
-      <div className="flex text-sm sm:text-base">© 2024 Ethan G.</div>
+      <div className="flex text-sm sm:text-base">© 2024 EG.</div>
       <div className="flex gap-4">
         <SocialIcons />
       </div>
